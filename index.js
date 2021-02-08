a453c8bdc57d748f3aef79d1531c8311f58dfc53
tobias-fletcher <75866568+tobias-fletcher@users.noreply.github.com>
2021-02-08 11:33:37 -0500

    Update Documentation/index
    - add documentation for endpoints/descriptions
    - add express code to index.js

--- index.js
+++ index.js
@@ -1,43 +1,72 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
 
 app.use(morgan('common'));
 
-let topMovies = [
+app.use(express.static('Public'));
+
+app.use(bodyParser.json());
+
+let movies = [
   {
-  title: 'You should have left'
+  title: 'You should have left',
+  director: 'Dave Koepp',
+  year: '2020'
+
 },
   {
-    title:'A quiet place'
+    title:'A quiet place',
+    director: 'John Krasinski',
+    year: '2018'
+  },
+
+  {
+    title: 'Black Box',
+    director: 'Emmanuel Osei-Kuffour Jr.',
+    year: '2020'
   },
   {
-    title: 'Black Box'
+    title: 'The invisible man',
+    director: 'Leigh Whannel',
+    year: '2020'
   },
   {
-    title: 'The invisible man'
+    title: 'Midsommar',
+    director: 'Ari Aster',
+    year: '2019'
   },
   {
-    title: 'Midsommar'
+    title: 'Antebellum',
+    director: 'Gerard Bush',
+    year: '2020'
   },
   {
-    title: 'Antebellum'
+    title: 'Ma',
+    director: 'Tate Taylor',
+    year: '2019'
   },
   {
-    title: 'Ma'
+    title: 'Us',
+    director: 'Jordan Peele',
+    year: '2019'
   },
   {
-    title: 'Us'
+    title: 'Hereditary',
+    director: 'Ari Aster',
+    year: '2018'
   },
   {
-    title: 'Hereditary'
+    title: 'Mother',
+    director: 'Darren Aronofsky',
+    year: '2017'
   },
   {
-    title: 'Mother'
-  },
-  {
-    title: 'Bird Box'
+    title: 'Bird Box',
+    director: 'Susanne Bier',
+    year: '2018'
   }
 ];
 
@@ -46,16 +75,50 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
+//get requests
 app.get('/', (req, res) => {
   res.send('Welcome to Horror Time');
 });
 
+//get list of data of movies
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
 });
 
-app.use(express.static('Public'));
+app.get('/movies/:title', (req, res) => {
+  res.json(movies.find((movie) => {
+    return movie.title === req.params.title
+  }));
+});
 
-app.listen(8011, () => {
-  console.log('App is listening on port 8011');
+app.get('/movies/year/:title', (req, res) => {
+  res.send('GET request successful - movie year by title')
+});
+
+app.get('/movies/directors/:name', (req, res) => {
+  res.send('GET request sucessful - movie director');
+});
+
+app.post('/users', (req, res) => {
+  res.send('POST request successful - new user registered');
+});
+
+app.put('/users/username', (req, res) => {
+  res.send('PUT request successful = updated information');
+});
+
+app.post('/users/:username/movies/:title', (req, res) => {
+  res.send('POST request successful - added movie to list');
+});
+
+app.delete('/user/:username/movies/:title', (res, req) => {
+  res.send('DELETE request successful - movie removed');
+});
+
+app.delete('/user/:username', (req, res) => {
+  res.send('DELETE request successful - user account terminated');
+});
+
+app.listen(8022, () => {
+  console.log('App is listening on port 8022');
 });
