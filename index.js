8d777d1cc6aeb381922923372bd93467bc19bb0e
tobias-fletcher <>
2021-05-04 18:43:37 -0500

    Update index.js
    - added config

--- index.js
+++ index.js
@@ -223,7 +223,6 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 // })
 
 const port = process.env.PORT || 8080;
-
-app.listen(Config.PORT, '0.0.0.0', () => {
-  console.log('Listening on Port ' + Config.PORT);
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port' + port);
 });
