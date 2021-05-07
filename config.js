a1b8df59a1c24d14a40783a5e63c6d133ea7159a
tobias-fletcher <>
2021-05-07 18:23:13 -0500

    Updated config.js
    - port 1234

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    PORT: process.env.PORT || 8080,
+    PORT: process.env.PORT || 1234,
     DB_CONNECT: process.env.DB_CONNECT || 'mongodb://localhost:27017/horror-time',
     JWT_SECRET: process.env.JWT_SECRET || 'temp_jwt_secret'
 }
