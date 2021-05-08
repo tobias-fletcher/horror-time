ee59fa47db940cec461a360114479cf38f3b95ca
tobias-fletcher <>
2021-05-08 09:54:12 -0500

    updated index.js
    Updated CORS policy

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ app.use(express.static('Public'));
 
 app.use(bodyParser.json());
 
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://itshorrortime.herokuapp.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const Movies = Models.Movie;
 const Users = Models.User;
