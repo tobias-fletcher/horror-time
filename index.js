999092b13c0126fa907bd7d505607cf8033a38af
tobias-fletcher <>
2021-05-17 11:40:28 -0500

    updated /movies to authenticate no matter what

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ app.use(morgan('common'));
 
 app.use(express.static('Public'));
 
-app.use(bodyParser.json());
-
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://itshorrortime.herokuapp.com/login', 'http://localhost:5000'];
+
 
 app.use(cors({
   origin: (origin, callback) => {
@@ -26,6 +25,8 @@ app.use(cors({
     return callback(null, true);
   }
 }));
+app.use(bodyParser.json());
+
 
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -43,7 +44,7 @@ app.get('/', (req, res) => {
 });
 
 //get list of data of movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movie) => {
       res.status(201).json(movie);
