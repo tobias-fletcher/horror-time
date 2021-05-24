552a1a7ece2995b5dd8008d0347256c2fb1accfa
tobias-fletcher <75866568+tobias-fletcher@users.noreply.github.com>
2021-05-24 12:36:34 -0500

    updated models
    - updated birthday requirement to string

--- models.js
+++ models.js
@@ -21,7 +21,7 @@ let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true },
-  Birthdate: Date,
+  Birthdate: { type: String },
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
