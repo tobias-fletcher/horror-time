0046b971bb62b261b638ebe3f7c35a2ad58879dd
tobias-fletcher <75866568+tobias-fletcher@users.noreply.github.com>
2021-05-25 19:41:37 -0500

    updated authorization
    - updated auth

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('Public'));
 
 app.use(bodyParser.json());
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://itshorrortime.herokuapp.com/login', 'http://localhost:5000'];
+let allowedOrigins = ['http://localhost:8080', 'https://itshorrortime.herokuapp.com/users', 'http://localhost:1234', 'http://localhost:1234', 'https://itshorrortime.herokuapp.com/login', 'http://localhost:5000'];
 
 app.use(cors({
   origin: (origin, callback) => {
