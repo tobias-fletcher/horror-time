22f9dcb2973bcefa04fcab173481ef3e15dac4c6
tobias-fletcher <tobias_fletcher@icloud.com>
2021-10-11 15:13:22 -0400

    updated cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('Public'));
 
 app.use(bodyParser.json());
 
-let allowedOrigins = ['http://localhost:8080', 'https://itshorrortime.herokuapp.com/users', 'http://localhost:1234', 'http://localhost:1234', 'https://itshorrortime.herokuapp.com/login', 'http://localhost:5000'];
+let allowedOrigins = ['http://localhost:8080', 'https://itshorrortime.herokuapp.com/users', 'http://localhost:1234', 'http://localhost:1234', 'https://itshorrortime.herokuapp.com/login', 'http://localhost:5000', 'https://zealous-banach-c1d750.netlify.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
